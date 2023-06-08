5d9e0e91d0e48704a8ba2ce274f8459bdf121a03
juzi <17621459431>
2023-06-09 05:52:05 +0800

    添加 user

--- swp/settings.py
+++ swp/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     # 'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 自定义模块
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +55,9 @@ ROOT_URLCONF = 'swp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "static")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +123,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    GENERS = (
+        ('male', '男性'),
+        ('female', '女性')
+    )
+    LOCATIONS = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('成都', '成都'),
+        ('武汉', '武汉'),
+    )
+    phonenum = models.CharField(max_length=16, unique=True, verbose_name='手机号')
+    nickname = models.CharField(max_length=20, db_index=True, verbose_name='昵称')
+    gender = models.CharField(max_length=10, choices=GENERS, default='male', verbose_name='性别')
+    birthday = models.DateField(default='2000-1-1', verbose_name='出生日')
+    avatar = models.CharField(max_length=256, verbose_name='个人形象')
+    location = models.CharField(max_length=10, choices=LOCATIONS, default='上海', verbose_name='常居地')
